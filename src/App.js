8141c75175c2cc9a53dede505be7143c12391a23
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2025-04-16 18:21:39 +0200

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useEffect, useReducer } from "react";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Loader from "./Components/Loader";
+import Error from "./Components/Error";
+import StartScreen from "./Components/StartScreen";
+import Question from "./Components/Question";
+const initialState = {
+  questions: [],
+  //loading,error,ready,active,finished
+  status: "loading",
+  index: 0,
+  answer: null,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed": {
+      return {
+        ...state,
+        status: "error",
+      };
+    }
+    case "start": {
+      return {
+        ...state,
+        status: "active",
+      };
+    }
+    case "newAnswer": {
+      return {
+        ...state,
+        answer: action.payload,
+      };
+    }
+    default:
+      throw new Error("Unknown action");
+  }
+}
+export default function App() {
+  const [{ status, questions, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  const numQuestions = questions.length;
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "dataReceieved", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
+      </Main>
     </div>
   );
 }
-
-export default App;
